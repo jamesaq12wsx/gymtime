5e92b53d387b4fefe57b8965d73624bf2b153524
ShaoChin Lin <jamesaq12wsx@gmail.com>
2020-03-08 23:52:16 -0700

    First Commit

---
+++ src/main/java/com/jamesaq12wsx/gymtime/GymtimeApplication.java
@@ -0,0 +1,13 @@
+package com.jamesaq12wsx.gymtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesaq12wsx/gymtime/GymtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesaq12wsx.gymtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
