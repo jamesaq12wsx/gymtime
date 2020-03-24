76d2495c766a4fea898206d65880e68c6814c389
ShaoChin Lin <jamesaq12wsx@gmail.com>
2020-03-23 22:32:45 -0700

    Club detail with user post records

--- src/main/java/com/jamesaq12wsx/gymtime/model/SimpleFitnessClubWithUserPost.java
+++ src/main/java/com/jamesaq12wsx/gymtime/model/SimpleFitnessClubWithUserPost.java
@@ -1,4 +1,32 @@
 package com.jamesaq12wsx.gymtime.model;
 
-public class SimpleFitnessClubWithUserPost {
+import com.google.common.collect.Lists;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class SimpleFitnessClubWithUserPost extends SimpleFitnessClub implements FitnessClub {
+
+    List<LocalDateTime> postDataTimeList;
+
+    public SimpleFitnessClubWithUserPost(UUID uuid, int brandId, String brandName, String name, int id, double latitude, double longitude, String address, String city, String state, String zipCode, String country, String homeUrl, Map<String, String> openHours, List<LocalDateTime> postDataTimeList) {
+        super(uuid, brandId, brandName, name, id, latitude, longitude, address, city, state, zipCode, country, homeUrl, openHours);
+        this.postDataTimeList = postDataTimeList;
+    }
+
+    public List<LocalDateTime> getPostDataTimeList() {
+        return postDataTimeList == null ? Collections.emptyList() : postDataTimeList;
+    }
 }
